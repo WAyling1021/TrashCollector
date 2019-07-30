c715d0e44d5d2d463ddb0634d3b20751d3fcd27d
WAyling1021 <william.ayling15@gmail.com>
2019-07-30 13:22:59 -0500

    init

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollectorProject2.Startup))]
+namespace TrashCollectorProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
