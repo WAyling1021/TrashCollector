1bc4fe7872d40cc9ccaddeefddfeb53478b72e46
WAyling1021 <william.ayling15@gmail.com>
2019-07-31 09:03:57 -0500

    Added a Customer and Employee class in the models folder

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject2.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string zipCode { get; set; }
+        public string pickupDay { get; set; }
+        public string OneTimePickup { get; set; }
+        public string bill { get; set; }
+        public string startAndEndDate { get; set; }
+        public string weeklyPickupDay { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorProject2.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string zipCode { get; set; }
+    }
+}
